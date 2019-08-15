8851c3584f98b2d06511c8e82f133a03259d0b24
Pimthip P <pimthip.p@pttdigital.com>
2019-08-15 21:45:59 +0700

    Implement blueprint for Line and Facebook

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
+
+    // Middleware 
+
+    next();
+});
+
 app.use('/', routes) 
 
 
